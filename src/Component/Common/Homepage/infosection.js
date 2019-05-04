e807b76b641b1a9d372de542f37b64e3a3c9b897
sivaram <sivarami@greatlearning.in>
2019-05-04 16:03:02 +0530

    Added new items

--- src/Component/Common/Homepage/infosection.js
+++ src/Component/Common/Homepage/infosection.js
@@ -6,6 +6,7 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import BackgroundImage from "./bg1.jpg";
+import { isPipelinePrimaryTopicReference } from '@babel/types';
 
 class Infosection extends React.Component {
   render() {
@@ -15,12 +16,12 @@ class Infosection extends React.Component {
             <Container maxWidth="lg">
                 
                 <Grid container spacing={3} alignItems="center">
-                    <Grid item sm={4} xs={12}>
-                        <Paper p={0} elevation={4}>
+                    <Grid item sm={5} xs={12} >
+                        <Paper p={0} elevation={12} className="card" bgcolor="primary.main" square={false} >
                            <img src={BackgroundImage}className="image-bg" />
                         </Paper>
                     </Grid>
-                    <Grid item sm={8} xs={12}>
+                    <Grid item sm={7} xs={12}>
                         <Paper p={2} elevation={0} className="category-card">
                             <Typography variant="h4" gutterBottom align="left">How this works</Typography>
                             <Typography variant="body1" gutterBottom align="left" color="textSecondary">Find what you need to live the college life to the fullest - job opportunities near campus, study abroad programs around the world, off-campus housing and sublet options, course notes from classmates, tutors in the area, and tons more right around your campus.</Typography>
